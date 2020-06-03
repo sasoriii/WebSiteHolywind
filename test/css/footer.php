6522e6e2abb0085308279b286cf75bbced8a3999
sasoriii <44494389+sasoriii@users.noreply.github.com>
2020-06-03 12:19:55 +0200

    Add files via upload

---
+++ test/css/footer.php
@@ -0,0 +1,47 @@
+<footer class="site-footer bg-dark mt-10">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12 col-md-6 mr-15" >
+        <h6>A propos</h6>
+        <p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </div>
+
+      <div class="col-xs-5 col-md-3" class="footer-links">
+          <h6>Liens Rapide</h6>
+          <a href="http://localhost">Accueil</a><p></p>
+          <a href="http://localhost/events/">Actualités</a><p></p>
+          <a href="http://localhost/boutique/">Boutique</a><p></p>
+      </div>
+
+      <div class="col-xs-6 col-md-2" class="footer-links">
+               <h6>Informations</h6>
+          <a href="http://localhost/mentions-legales/">Mentions légales</a><p></p>
+          <a href="http://localhost/mentions-legales/cookieInfo.php">A propos des cookies</a><p></p>
+          <a href="http://localhost/mentions-legales/cgv.php">Conditions générale de vente</a><p></p>
+          <a href="http://localhost/contact.php">Nous contacter</a><p></p>
+      
+      </div>
+    </div>
+    <hr>
+  </div>
+
+  <div class="col-md-8" class="social-icons">
+
+      <a class="facebook" href="https://www.facebook.com/cabrinhafrance/"><i class="mif-facebook"></i></a>
+      <a class="twitter" href="#"><i class="mif-twitter"></i></a>
+      <a class="linkedin" href="#"><i class="mif-linkedin"></i></a>
+      <a class="youtube" href="#"><i class="mif-youtube"></i></a>
+      <a class="instagram" href="https://www.instagram.com/cabrinhakites/?hl=fr"><i class="mif-instagram"></i></a>
+    
+  </div>
+    <div style="text-align: left">
+    <pre>
+        <?php
+            print_r($_SESSION);
+        ?>
+    </pre>
+    </div>
+
+</div>
+</div>
+</footer>
---
+++ test/css/header.php
@@ -0,0 +1,21 @@
+<div data-role="appbar" data-expand-point="md" class="w-200 bg-violet fg-black">
+	<a href="http://localhost/projetperso/index.php" class="homeLogo" >
+	<img class="homeLogoImg mt-2 ml-2 " src="/img/logo.png">
+		
+	</a>
+
+	<ul class="app-bar-menu">
+		<li><a href="../index.php">Accueil</a></li>
+		<li><a href="../shop/shop.php">Boutique</a></li>
+		<li><a href="https://www.cabrinha.com/">Cabrinha</a></li>
+        <li><a href="../test/chat/chat.php">Chat</a></li>
+		<li><a href="../panier/panier.php">Panier</a></li>
+	</ul>
+	<div class="app-bar-container ml-auto" data-flexdirection="reverse"></div>
+    <span class="mif-1x mif-user fg-black"><a href="../profile/connexion.php">Mon compte / connexion</a> </span>
+	</div>
+	
+	
+</div>
+
+<div class="pt-18"></div>
